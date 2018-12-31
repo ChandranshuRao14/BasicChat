43fb066fb1efbfc3a2eafabbd74ecea419de4e68
ChandranshuRao14 <chandranshu.rao@gmail.com>
2018-12-31 06:58:05 -0600

    redo chat app

--- server.js
+++ server.js
@@ -3,30 +3,16 @@ var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 const port = 8080;
-var router = express.Router();  
 
-app.use('/', router);
-
-var welcome = {
-    data: "welcome to the BasicChatAPI"
-};
-
-var obj = {
-    data: [{"name": "song1"}]
-};
+app.use(express.static('public'));
 
 server.listen(port, () => 
     console.log(`BasicChat Server listening on port ${port}...`)
 );
 
-router.get('/', (req, res) => {
-    res.json(welcome);
+app.get('/', (req, res) => {
+    res.res.sendFile(__dirname + '/public/index.html')
 });
-
-router.route('/rooms')
-    .get((req, res) => {
-        res.json(obj);
-    });
 
 io.on('connection', function(client) {
     console.log('Client connected...');
@@ -35,10 +21,8 @@ io.on('connection', function(client) {
         console.log(data);
     });
 
-    client.on('songs', function(data){
-        console.log(data);
-        obj.data.push({name: data});
-        client.emit('queue', data);
-        client.broadcast.emit('queue', data);
+    client.on('messages', function(data){
+        client.emit('thread', data);
+        client.broadcast.emit('thread', data);
     });
 });
